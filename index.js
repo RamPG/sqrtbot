880be2e32c9dc00a21cbfce17a904bdb68d7f9c5
RamPG <vlasovnew@yandex.ru>
2021-09-13 18:06:25 +0500

    release 1.0

--- index.js
+++ index.js
@@ -1,13 +1,112 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = require('./token').token;
+const fs = require('fs');
+const outputMessageLanguage = require('./languages');
+
+const rawData = fs.readFileSync('./users.json');
+const users = JSON.parse(rawData);
+const rawToken = fs.readFileSync('./token.json');
+const {token} = JSON.parse(rawToken);
 
 const bot = new TelegramBot(token, {polling: true});
+const writeUsersList = () => {
+    const data = JSON.stringify(users);
+    fs.writeFileSync('./users.json', data);
+}
+const findUserId = (fromUserId) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].userId == fromUserId) {
+            return i;
+        }
+    }
+    return '';
+}
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+const updateUsersList = (fromUserId, language) => {
+    users.push({
+        userId: fromUserId,
+        language: language,
+    });
+    writeUsersList();
+}
+const getLanguageUser = (fromUserId) => {
+    const id = findUserId(fromUserId);
+    if (id === '') {
+        updateUsersList(fromUserId, 'en');
+        return 'en';
+    }
+    return users[id].language;
+}
+const setLanguageUser = (fromUserId, language) => {
+    const id = findUserId(fromUserId);
+    if (id === '') {
+        updateUsersList(fromUserId, language);
+    }
+    users[id].language = language;
+    writeUsersList()
+}
+
+const getMessageLanguage = (language, command) => {
+    return outputMessageLanguage[language][command];
+}
+bot.on("polling_error", console.log);
+bot.onText(/\/start/, (msg) => {
+    const {id} = msg.from;
+    let language = getLanguageUser(id);
+    const outputMessage = getMessageLanguage(language, 'start');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/ru/, (msg) => {
+    const {id} = msg.from;
+    setLanguageUser(id, 'ru');
+    const outputMessage = getMessageLanguage('ru', 'language');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/en/, (msg) => {
+    const {id} = msg.from;
+    setLanguageUser(id, 'en');
+    const outputMessage = getMessageLanguage('en', 'language');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/es/, (msg) => {
+    const {id} = msg.from;
+    setLanguageUser(id, 'es');
+    const outputMessage = getMessageLanguage('es', 'language');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/zh/, (msg) => {
+    const {id} = msg.from;
+    setLanguageUser(id, 'zh');
+    const outputMessage = getMessageLanguage('zh', 'language');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/faq/, (msg) => {
+    const {id} = msg.from;
+    let language = getLanguageUser(id);
+    const outputMessage = getMessageLanguage(language, 'faq');
+    bot.sendMessage(id, outputMessage);
+});
+bot.onText(/\/call/, (msg) => {
+    const {id} = msg.from;
+    let language = getLanguageUser(id);
+    const outputMessage = getMessageLanguage(language, 'call');
+    bot.sendMessage(id, outputMessage);
+    bot.sendMessage(1099874743, 'Заявка на обслуживание. Язык: ' + language + ' id пользователя: ' + id);
+});
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+    const {id} = msg.from;
+    let language = getLanguageUser(id);
+    if (id != 1099874743) {
+        bot.sendMessage(1099874743, 'Язык: ' + language + ' id пользователя: ' + id + ' Сообщение: ' + msg.text);
+    }
+});
+bot.onText(/\/operator/, (msg) => {
+    const message = msg.text.split(' ');
+    const id = parseInt(message[1]);
+    const text = message.slice(2, message.length).join(' ');
+    if (msg.from.id == 1099874743) {
+        bot.sendMessage(id, text);
+    } else {
+        bot.sendMessage(msg.from.id, 'У вас нет прав.');
+    }
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
 });
---
+++ languages.js
@@ -0,0 +1,82 @@
+module.exports = {
+    ru: {
+        start: 'Здравствуйте, это служба поддержки sqrtCo. \n'
+            +
+            'Навигация: \n'
+            +
+            '/ru - изменить язык на русский \n'
+            +
+            '/en - изменить язык на английский \n'
+            +
+            '/zh - изменить язык на китайский \n'
+            +
+            '/es - изменить язык на испанский \n'
+            +
+            '/faq - часто задаваемые вопросы \n'
+            +
+            '/call - вызвать оператора',
+        language: 'Вы успешно сменили язык на русский',
+        faq: 'Всю информацию по работе с программой вы можете найти, нажав кнопку руководство пользователя на главной странице',
+        call: 'Ожидание оператора... С вами скоро свяжутся!'
+    },
+    es: {
+        start: 'Hola, este es el Servicio de soporte de sqrt Co. \n'
+            +
+            'Navegación: \n'
+            +
+            '/ru - cambiar el idioma al ruso \n'
+            +
+            '/en - cambiar idioma a Inglés \n'
+            +
+            '/zh - cambiar idioma a chino \n'
+            +
+            '/es - cambiar el idioma al español \n'
+            +
+            '/FAQ - preguntas frecuentes \n'
+            +
+            '/call - llamar al operador',
+        language: 'Has cambiado tu idioma al español con éxito',
+        faq: 'Puede encontrar toda la información sobre cómo trabajar con el programa haciendo clic en el botón manual del usuario en la página principal',
+        call: 'Esperando a la operadora... Usted será contactado pronto!',
+    },
+    en: {
+        start: 'Hello, this is the support service of sqrtCo. \n'
+            +
+            'Navigation: \n'
+            +
+            '/ru - change the language to Russian \n'
+            +
+            '/en - change the language to English \n'
+            +
+            '/zh - change the language to Chinese \n'
+            +
+            '/es - change the language to spanish \n'
+            +
+            '/FAQ - Frequently asked Questions \n'
+            +
+            '/call - call the operator',
+        language: 'You have successfully changed your language to English',
+        faq: 'You can find all the information on working with the program by clicking the user manual button on the main page',
+        call: 'Waiting for the operator... You will be contacted soon!'
+    },
+    zh: {
+        start: '您好，这是sqrtCo的支持服务. \n'
+            +
+            '导航: \n'
+            +
+            '/ru - 将语言更改为俄语 \n'
+            +
+            '/en - 将语言更改为英语 \n'
+            +
+            '/zh - 将语言更改为中文 \n'
+            +
+            '/es - 将语言更改为西班牙语 \n'
+            +
+            '/FAQ - 常见问题 \n'
+            +
+            '/call - 呼叫接线员',
+        language: '您已成功将您的语言更改为英语',
+        faq: '您可以通过单击主页上的用户手册按钮找到有关使用该程序的所有信息',
+        call: '等待操作员。.. 您将很快与我们联系'
+    }
+}
